3eb2ee54adcdd015e9bd17629057c2d36276c585
Hiu1975 <hubertdrozda75@gmail.com>
2024-02-18 19:06:04 +0100

    Added Test class - empty class Modified Program - modified line 12

--- HelloWorldzie/Program.cs
+++ HelloWorldzie/Program.cs
@@ -9,4 +9,4 @@ double addResult = math.Add(a, b);
 double substractResult =math.Subtract(a, b);
 
 Console.WriteLine(addResult);
-Console.WriteLine(substractResult);
+Console.WriteLine(substractResult + " ");
