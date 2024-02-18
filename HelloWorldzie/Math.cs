bc82b2ede5f215a9fc3112f9579d9b61e2ba1097
Hiu1975 <hubertdrozda75@gmail.com>
2024-02-18 18:09:23 +0100

    Add project files.

---
+++ HelloWorldzie/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldzie
+{
+    public class Math1
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ HelloWorldzie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using HelloWorldzie;
+
+double a = 20, b = 10;
+
+Math1 math = new Math1();
+
+double addResult = math.Add(a, b);
+double substractResult =math.Subtract(a, b);
+
+Console.WriteLine(addResult);
+Console.WriteLine(substractResult);
